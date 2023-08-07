b6a534c33f26172313b9a6191580daee39bbffba
ankita-barad <ankitabarad2111@gmail.com>
2023-08-07 14:47:36 +0530

    json_server

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const jsonserver = require("json-server");
+const cors = require("cors");
+const server = jsonserver.create();
+const router = jsonserver.router("data.json");
+
+server.use(cors());
+server.use(jsonserver.bodyParser);
+server.use(router);
+
+server.listen(process.env.PORT, () => {
+  console.log(`JSON server is running on port ${process.env.PORT}`);
+});
