e2c2d5564309ca0ac01316d3a61be3775eb6a161
ankita-barad <ankitabarad2111@gmail.com>
2023-08-07 14:48:39 +0530

    json_server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const jsonserver = require("json-server");
 const cors = require("cors");
 const server = jsonserver.create();
-const router = jsonserver.router("data.json");
+const router = jsonserver.router("db.json");
 
 server.use(cors());
 server.use(jsonserver.bodyParser);
